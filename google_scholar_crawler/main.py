b76f32c95c38437b4146d967bb3d9b176730e25e
weiyi6 <siyouweiyi66@gmail.com>
2026-03-24 14:47:03 +0800

    update

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -1,17 +1,8 @@
-from scholarly import scholarly, ProxyGenerator
+from scholarly import scholarly
 import jsonpickle
 import json
 from datetime import datetime
 import os
-
-# 配置代理池，防止被谷歌学术封锁 IP
-pg = ProxyGenerator()
-# print("正在寻找可用的免费代理，这可能需要几分钟...")
-# pg.FreeProxies()
-# scholarly.use_proxy(pg)
-# print("代理配置完成，开始抓取数据...")
-success = pg.ScraperAPI(os.environ.get('SCRAPER_API_KEY'))
-scholarly.use_proxy(pg)
 
 # 直接定义GOOGLE_SCHOLAR_ID
 GOOGLE_SCHOLAR_ID = "ZKXXk4IAAAAJ"
